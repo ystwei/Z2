09d0fd29a815b8b67b4885f1ec72347f4bc5dea6
weikun <wk2003119@163.com>
2017-03-14 18:26:55 +0800

    314

---
+++ js/1.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Administrator on 2017/3/14.
+ */
+function ok(me){
+    alert(document.links[1].href);
+}
+function f(){
+    document.write('123');
+
+}
+function s(t){
+    alert(t);
+    if(t==false){
+        return false;
+
+    }else{
+        return true;
+    }
+}
